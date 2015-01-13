adba0f3819be91a9246ab843369307f9349f2f83
Stephen Murby <stephen.murby@autotrader.co.uk>
2015-01-13 11:35:17 +0000

    fixed script and template imports

--- static/js/app.js
+++ static/js/app.js
@@ -2,20 +2,56 @@
  * Created by stephen.murby on 1/13/2015.
  */
 
-var directoryApp = angular.module("directory",[]);
+var directoryApp = angular.module("directory",['ngRoute']);
+
+directoryApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/list', {
+        templateUrl: 'list.html'
+    }).otherwise({
+            redirectTo: '/list'
+        })
+}]);
 
 directoryApp.directive("aceList", [function() {
-
+    return {
+        templateUrl: "ui/directory/ace-list.html",
+        restrict: 'E',
+        scope: {},
+        controller: function($scope, $iElement, $iAttributes) {
+            return;
+        }
+    }
 }]);
 
 directoryApp.directive("aceInput", [function() {
-
+    return {
+        templateUrl: "ui/common/ace-input.html",
+        restrict: 'E',
+        scope: {},
+        controller: function($scope, $iElement, $iAttributes) {
+            return;
+        }
+    }
 }]);
 
 directoryApp.directive("aceButton", [function() {
-
+    return {
+        templateUrl: "ui/common/ace-button.html",
+        restrict: 'E',
+        scope: {},
+        controller: function($scope, $iElement, $iAttributes) {
+            return;
+        }
+    }
 }]);
 
 directoryApp.directive("aceErrorMessage", [function() {
-
+    return {
+        templateUrl: "ui/common/ace-error-message.html",
+        restrict: 'E',
+        scope: {},
+        controller: function($scope, $iElement, $iAttributes) {
+            return;
+        }
+    }
 }]);
