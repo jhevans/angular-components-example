be63f9cb005ed8ee8ae51e5b4fc0d3872dafda5e
Stephen Murby <stephen.murby@autotrader.co.uk>
2015-01-13 10:45:06 +0000

    Added angular dependencies

---
+++ static/js/app.js
@@ -0,0 +1,21 @@
+/**
+ * Created by stephen.murby on 1/13/2015.
+ */
+
+var directoryApp = angular.module("directory",[]);
+
+directoryApp.directive("aceList", [function() {
+
+}]);
+
+directoryApp.directive("aceInput", [function() {
+
+}]);
+
+directoryApp.directive("aceButton", [function() {
+
+}]);
+
+directoryApp.directive("aceErrorMessage", [function() {
+
+}]);
