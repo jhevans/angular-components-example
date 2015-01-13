78d9ebe42373f008e4668be83a295ef0cd166ffe
Stephen Murby <stephen.murby@autotrader.co.uk>
2015-01-13 16:14:38 +0000

    Added angular-data and angular-mocks, resource requests mocked out

--- static/js/app.js
+++ static/js/app.js
@@ -2,46 +2,63 @@
  * Created by stephen.murby on 1/13/2015.
  */
 
-var directoryApp = angular.module("directory",['ngRoute']);
+var directoryApp = angular.module("directory",['ngRoute', 'angular-data.DS', 'ngMockE2E']);
 
 directoryApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
-    when('/list', {
-        templateUrl: 'list.html'
+    when('/records', {
+        templateUrl: 'partials/list.html'
     }).
-    when('/edit', {
-        templateUrl: "edit.html"
+    when('/records/create', {
+        templateUrl: "partials/edit.html"
+    }).
+    when('/records/:recordId', {
+        templateUrl: "partials/edit.html"
     }).
     otherwise({
-        redirectTo: '/list'
+    redirectTo: '/records'
     });
 }]);
 
-directoryApp.directive("aceList", [function() {
+directoryApp.directive("aceList", ['$location', function($location) {
     return {
         templateUrl: "ui/directory/ace-list.html",
         restrict: 'E',
         scope: {},
         controller: function($scope) {
-            return;
+            $scope.create = function() {
+                $location.path('records/create');
+            };
         }
     }
 }]);
 
-directoryApp.directive("aceEdit", ['$window', function() {
+directoryApp.directive("aceEdit", ['$window', '$routeParams', 'Record', function($window, $routeParams, Record) {
     return {
         templateUrl: "ui/directory/ace-edit.html",
         restrict: 'E',
         scope: {},
-        controller: function($scope, $window) {
+        controller: ['$scope', function($scope) {
+            $scope.record = null;
+
+            function setRecord(record){
+                $scope.record = record;
+            };
+
+            if($routeParams.recordId !== undefined) {
+                Record.find($routeParams.recordId).then(setRecord);
+            };
+
             $scope.cancel = function() {
                 $window.history.back();
             };
 
-            $scope.submit = function() {
-
-            }
-        }
+            $scope.save = function() {
+                debugger
+                console.log("Saving to record " + $scope.recordId);
+                Record.save($rootParams.recordId, $scope.record);
+            };
+        }]
     }
 }]);
 
@@ -77,4 +94,44 @@ directoryApp.directive("aceErrorMessage", [function() {
             return;
         }
     }
+}]);
+
+directoryApp.factory('Record', ['DS', function(DS) {
+    var Record = DS.defineResource({
+        name: 'record',
+        idAttribute: 'id',
+        endpoint: 'records'
+    });
+    return Record;
+}]);
+
+
+
+// Everything following this comment is just mocking out the backend
+
+directoryApp.run(['$httpBackend', function($httpBackend){
+    var DEFAULT_RECORDS = [{
+        id: 1,
+        name: 'John Evans',
+        number: '01928 356115'
+    },{
+        id: 16,
+        name: 'Murbs',
+        number: '53180 080000'
+    }];
+
+    $httpBackend.whenGET('records').respond(DEFAULT_RECORDS);
+    $httpBackend.whenGET('records/1').respond(DEFAULT_RECORDS[0]);
+    $httpBackend.whenGET('records/16').respond(DEFAULT_RECORDS[1]);
+
+    $httpBackend.whenPUT('records').respond(function(method, url, data) {
+//        var vehicle = angular.fromJson(data);
+        return [200, data, {}];
+    });
+    $httpBackend.whenDELETE(/records\/\d*/).respond(function(method, url, data) {
+//        var vehicle = angular.fromJson(data);
+        return [200, data, {}];
+    });
+    $httpBackend.whenGET(/partials/).passThrough()
+    $httpBackend.whenGET(/ui/).passThrough()
 }]);
